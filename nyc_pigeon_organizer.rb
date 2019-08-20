7b580c8ee54bca23ced701c305442529146b70f6
lyle dickie <lyle.e.dickie@gmail.com>
2019-08-20 18:55:02 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,15 +1,15 @@
 def nyc_pigeon_organizer(data)
-  hsh = {}
+  hsh = Hash.new
   
   data.each do |key, value|
     value.each do |attribute, names|
       names.each do |bird|
         unless hsh[bird]
-          hsh[bird] = {}
+          hsh[bird] = Hash.new
         end
         
         unless hsh[bird][key]
-          hsh[bird][key] = []
+          hsh[bird][key] = Array.new
         end
         
         hsh[bird][key] << attribute.to_s
