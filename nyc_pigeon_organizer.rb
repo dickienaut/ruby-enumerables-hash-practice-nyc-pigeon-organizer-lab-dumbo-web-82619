ab9e702bbc4c7cd1302d3c3ca483c0f3cf8c2bdc
lyle dickie <lyle.e.dickie@gmail.com>
2019-08-20 18:49:59 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,6 +4,7 @@ def nyc_pigeon_organizer(data)
   data.each do |key, value|
     value.each do |attribute, names|
       names.each do |bird|
-        unless hsh[bird] 
+        unless hsh[bird]
+          
   
 end
